f2ba2a56e07691f927e502634247b66c302cbe19
Zmyanma <131891724+Zmyanma@users.noreply.github.com>
2023-08-22 18:04:29 +0500

    added task page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Link, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Info from "./pages/Info";
 import About from "./pages/About";
+import Task from "./pages/Task";
 
 function App() {
   return (
@@ -18,11 +19,13 @@ function App() {
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/info">Info</Link>
+        <Link to="/task">Task</Link>
       </div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/info" element={<Info />} />
         <Route path="/about" element={<About />} />
+        <Route path="/task" element={<Task />} />
       </Routes>
     </div>
   );
